f4d540f0e1b276d5abf6113b0d583b1a957ff081
yusufmr666 <63086518+yusufmr666@users.noreply.github.com>
2020-05-28 13:50:10 +0700

    fix

--- tugascuy/app/src/main/java/com/example/tugascuy/rest/ApiConfig.java
+++ tugascuy/app/src/main/java/com/example/tugascuy/rest/ApiConfig.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiConfig {
     public static ApiService getApiService() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.43.164/TrainingPHP/")
+                .baseUrl("http://192.168.43.164/lesKohDerry/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
